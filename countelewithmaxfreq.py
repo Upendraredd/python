32173631fa1d5250f1677067afae077486fc85de
peddappagari upendra reddy <97207144+Upendraredd@users.noreply.github.com>
2024-03-19 13:19:21 +0530

    Create countelewithmaxfreq.py

---
+++ countelewithmaxfreq.py
@@ -0,0 +1,7 @@
+class Solution:
+    def maxFrequencyElements(self, nums: List[int]) -> int:
+        counter = Counter(nums)
+        max_freq = max(counter.values())
+        return sum(freq for freq in counter.values() if freq == max_freq)
+        # nums.sort()
+        # print(max(nums))
